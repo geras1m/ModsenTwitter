8139675b59d30a7aae8db1b8bed2eb816ec44b56
Anton Goldovsky <goldovsky.anton@gmail.com>
2024-04-18 22:16:04 +0300

    feat: add TweetSearchPage for showing search result

---
+++ src/pages/TweetSearchPage/index.tsx
@@ -0,0 +1,72 @@
+import { useCallback, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Unsubscribe } from 'firebase/firestore';
+
+import { TweetItem } from '@/components/TweetItem';
+import { useAppSelector } from '@/hooks/reduxHooks';
+import { TweetSearchPageWrapper } from '@/pages/TweetSearchPage/styled';
+import { FirebaseService } from '@/service';
+import { ITweetData } from '@/types';
+
+export const TweetSearchPage = () => {
+  const { id } = useAppSelector((state) => state.user);
+  const { id: paramId } = useParams();
+  const [tweet, setTweet] = useState<ITweetData | null>(null);
+
+  useEffect(() => {
+    let unsubscribe: Unsubscribe = () => {};
+    if (paramId) {
+      const subscribeOnTweetsUpdate = async () =>
+        FirebaseService.SubscriptionOnTweetUpdateForSearchPage(paramId, setTweet);
+
+      subscribeOnTweetsUpdate().then((unsubscribeFunction) => {
+        unsubscribe = unsubscribeFunction;
+      });
+    }
+    return () => {
+      unsubscribe();
+    };
+  }, [paramId]);
+
+  const handleRemoveTweet = async () => {
+    if (tweet) {
+      try {
+        await FirebaseService.RemoveTweet(tweet.uis.toString());
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  };
+
+  const handleChangeLike = useCallback(async (tweetId: string | number, userId: string) => {
+    try {
+      await FirebaseService.ChangeTweetLike(userId, tweetId.toString());
+    } catch (e) {
+      console.log(e);
+    }
+  }, []);
+
+  const likesCount = tweet?.likes.length;
+  const isActiveLikeIcon = tweet?.likes.indexOf(id!) !== -1;
+
+  return (
+    <TweetSearchPageWrapper>
+      {tweet && (
+        <TweetItem
+          uis={tweet.uis}
+          authorId={tweet.authorId}
+          date={tweet.date}
+          text={tweet.text}
+          imgLink={tweet.imgLink}
+          authorName={tweet.authorName}
+          authorTag={tweet.authorTag}
+          userId={id!.toString()}
+          handleRemoveTweet={handleRemoveTweet}
+          handleChangeLike={handleChangeLike}
+          likes={likesCount!}
+          isActiveLikeIcon={isActiveLikeIcon}
+        />
+      )}
+    </TweetSearchPageWrapper>
+  );
+};
---
+++ src/pages/TweetSearchPage/styled.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const TweetSearchPageWrapper = styled.main`
+  width: 60%;
+  max-width: 910px;
+  border: 1px solid #afafaf;
+`;
