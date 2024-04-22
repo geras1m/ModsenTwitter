511365aa473a21fef006ab99ce122cc0d4a37c6c
Anton Goldovsky <goldovsky.anton@gmail.com>
2024-04-22 18:01:22 +0300

    feat: add plugin for removing tests attributes

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,20 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
 import dotenv from 'dotenv';
+import removeAttr from 'react-remove-attr';
 import path from 'path';
 
 dotenv.config();
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    removeAttr({
+      extensions: ['jsx', 'tsx'],
+      attributes: ['data-testid'],
+    }),
+    react(),
+    svgr(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
