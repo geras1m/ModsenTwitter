c134350439312e803473e2329f18e0d1c77295a6
Anton Goldovsky <goldovsky.anton@gmail.com>
2024-04-26 19:24:08 +0300

    feat: add the spinner for persist gate

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,8 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 import { App } from '@/components/App';
 import { ErrorBoundary } from '@/components/ErrorBoundary';
+import { BackgroundLoader } from '@/components/Router/styled';
+import { Spinner } from '@/components/Spinner';
 import { ThemeProvider } from '@/components/ThemeProvider';
 import { ToastProvider } from '@/context/ToastProvider';
 import { store } from '@/store';
@@ -19,7 +21,14 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate
-        loading={<p>Loading...</p>}
+        loading={
+          <BackgroundLoader>
+            <Spinner
+              width='100px'
+              border='10px'
+            />
+          </BackgroundLoader>
+        }
         persistor={persistor}
       >
         <ErrorBoundary>
